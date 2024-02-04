e7d8f55f1f09721d808e3946930c000361e3fcc3
Denellyne <gustavobarbosavs@gmail.com>
2024-02-04 18:25:32 +0000

    Update to Trigger Editor (More user friendly)

--- src/User Settings/Adaptive Triggers/Adaptive Triggers.cpp
+++ src/User Settings/Adaptive Triggers/Adaptive Triggers.cpp
@@ -1,7 +1,30 @@
 #include "Adaptive Triggers.h"
 #include "GUI\Functions\Misc\functionality.h"
 #include <fstream>
+#include <map>
 
+static std::map<int, const char*> triggerModes = {
+		{0x0,"Off"},
+		{0x1,"Rigid"},
+		{0x2,"Pulse"},
+		{0x1 | 0x20,"Rigid Extra 1"},
+		{0x1 | 0x04,"Rigid Extra 2"},
+		{0x1 | 0x20 | 0x04,"Rigid Complete Mode"},
+		{0x2 | 0x20,"Pulse Extra 1"},
+		{0x2 | 0x04,"Pulse Extra 2"},
+		{0x2 | 0x20 | 0x04,"Pulse Complete Mode"},
+};
+
+static const short int triggerModeKeys[9] = { 
+												0x0, //Off
+												0x1, //Rigid
+												0x2, //Pulse
+												0x1 | 0x20, //Rigid_A
+												0x1 | 0x04 ,  //Rigid_B
+												0x1 | 0x20 | 0x04 , //Rigid AB
+												0x2 | 0x20, //Pulse_A
+												0x2 | 0x04,  //Pulse_B
+												0x2 | 0x20 | 0x04 }; //Pulse AB
 
 extern unsigned char ptrCurrentTriggerProfile[8]{};
 extern bool gameProfileSet;
@@ -43,16 +66,6 @@ void triggerEditor(bool& makerOpen, unsigned char* trigger,bool& rumbleTriggers)
 	rumble[0] = 0;
 	rumble[1] = 0;
 
-	static const short int triggerModes[9] = { 0x0, //Off
-												0x1, //Rigid
-												0x2, //Pulse
-												0x1 | 0x20, //Rigid_A
-												0x1 | 0x04 ,  //Rigid_B
-												0x1 | 0x20 | 0x04 , //Rigid AB
-												0x2 | 0x20, //Pulse_A
-												0x2 | 0x04,  //Pulse_B
-												0x2 | 0x20 | 0x04 }; //Pulse AB
-
 	if (ImGui::Begin("Adaptive Trigger Maker", &makerOpen)) {
 		static int motor1{}, motor2{};
 
@@ -60,20 +73,21 @@ void triggerEditor(bool& makerOpen, unsigned char* trigger,bool& rumbleTriggers)
 		memcpy(&ptrCurrentTriggerProfile, &*trigger, 8);
 		
 
-		if (ImGui::BeginCombo("Trigger Mode", std::format("{}", (int)trigger[0]).c_str())) {
-			for (short int i = 0; i < IM_ARRAYSIZE(triggerModes); i++) if (ImGui::Selectable(std::format("{}", triggerModes[i]).c_str())) trigger[0] = triggerModes[i];
+		if (ImGui::BeginCombo("Trigger Mode", triggerModes[trigger[0]])) {
+			for (short int i = 0; i < IM_ARRAYSIZE(triggerModeKeys); i++) if (ImGui::Selectable(triggerModes[triggerModeKeys[i]])) trigger[0] = triggerModeKeys[i];
 			ImGui::EndCombo();
 		}
 		if (ImGui::BeginItemTooltip()) {
-			ImGui::Text("0 = Off\n1 = Rigid\n2 = Pulse\n33 = Rigid_A\n5 = Rigid_B\n37 = Rigid_AB\n34 = Pulse_A\n6 = Pulse_B - Rumble affects Triggers\n38 = Pulse_AB\n");
+			ImGui::Text("Pulse Extra 2 adds option for Rumble Sensitive Triggers");
 			ImGui::EndTooltip();
 		}
 
 		if (trigger[0] == (0x2 | 0x04)) {
-			if (ImGui::RadioButton("Dinamic Rumble to Triggers", rumbleTriggers)) 
+			if (ImGui::RadioButton("Dinamic Rumble to Triggers", rumbleTriggers))
 				rumbleTriggers = !rumbleTriggers;
 
 			if (rumbleTriggers) {
+				ZeroMemory(&trigger[3], 0);
 
 				ImGui::SliderInt("##2", (int*)&trigger[1], 0, 15);
 				ImGui::SliderInt("##3", (int*)&trigger[2], 0, 200);
