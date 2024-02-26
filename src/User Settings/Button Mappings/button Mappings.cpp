33d279fc12a6315699202493c046016a33192e7a
Denellyne <gustavobarbosavs@gmail.com>
2024-02-26 13:31:43 +0000

    Revert "Refactored"
    This reverts commit 86ab29a50a427968e8a107add1639ab1620bac77.

--- src/User Settings/Button Mappings/button Mappings.cpp
+++ src/User Settings/Button Mappings/button Mappings.cpp
@@ -7,40 +7,41 @@ static std::map<int, const char*> xboxButtons = {
 	{0x1000,"A"},
 	{0x2000,"B"},
 	{0x8000,"Y"},
-	{0x0100,"Left Shoulder"},
-	{0x0200,"Right Shoulder"},
+	{0x0100,"LB"},
+	{0x0200,"RB"},
 	{0x0020,"Back"},
 	{0x0010,"Start"},
-	{0x0040,"Left Thumb"},
-	{0x0080,"Right Thumb"},
+	{0x0040,"LSB"},
+	{0x0080,"RSB"},
 };
 
 static const char* dualsenseEdgeExtras[4]{
-	"Left Function ",
-	"Right Function",
-	"Left Paddle   ",
-	"Right Paddle  ",
+	"L Fn    ",
+	"R Fn    ",
+	"L Back  ",
+	"R Back  ",
 };
 
-static const char* psButtons[12]{
-	"Square        ",
-	"Cross         ",
-	"Circle        ",
-	"Triangle      ",
-	"L1            ",
-	"R1            ",
-	"Share         ",
-	"Options       ",
-	"L3            ",
-	"R3            ",
-	"Home          ",
-	"Mic           ",
+static const char* psButtons[13]{
+	"Square  ",
+	"Cross   ",
+	"Circle  ",
+	"Triangle",
+	"L1      ",
+	"R1      ",
+	"Share   ",
+	"Options ",
+	"L3      ",
+	"R3      ",
+	"PS      ",
+	"Touchpad",
+	"Mic     ",
 };
 
 static const int xboxButtonsKeys[11]{ 0x0000,0x4000,0x1000,0x2000,0x8000,0x0100,0x0200,0x0020,0x0010,0x0040,0x0080 };
 
 void buttonMappingEditor(bool& makerOpen, int* buttonProfile) {
-	extern int buttonMapping[18];
+	extern int buttonMapping[19];
 	extern bool gameProfileSet;
 
 	for (int i = 0; i < IM_ARRAYSIZE(buttonMapping); i++) buttonMapping[i] = buttonProfile[i];
@@ -58,6 +59,7 @@ void buttonMappingEditor(bool& makerOpen, int* buttonProfile) {
 			}
 			ImGui::PopID();
 		}
+
 		for (short int i = 0; i < IM_ARRAYSIZE(dualsenseEdgeExtras); i++) {
 			ImGui::PushID(&buttonProfile[i + 15]);
 			ImGui::Text(dualsenseEdgeExtras[i]);
